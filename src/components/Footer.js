700c6ea5f5a51a82247c76b1dc28eec7fc900ea9
sanjaybhatt2002 <133907679+sanjaybhatt2002@users.noreply.github.com>
2023-09-09 12:33:16 +0530

    YumEats Frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react' 
+import { FaDribbbleSquare, FaFacebookSquare, FaGithubSquare, FaInstagram, FaTwitterSquare } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1520px] m-auto px-4 py-2 bg-[#24262b]">
+        <div className="py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+            <div>
+                <h1 className="w-full text-3xl font-bold text-orange-500">YumEats</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A iste sapiente nesciunt ipsa, sed est, ipsum et soluta consectetur ea amet nihil, ratione debitis numquam temporibus fugit harum voluptatibus porro?
+                </p>
+                <div className="flex justify-between md:w-[75%] my-6">
+                    <FaFacebookSquare size={30} className="cursor-pointer hover:scale-105"/>
+                    <FaInstagram size={30} className="cursor-pointer hover:scale-105"/>
+                    <FaTwitterSquare size={30} className="cursor-pointer hover:scale-105"/>
+                    <FaGithubSquare size={30} className="cursor-pointer hover:scale-105"/>
+                    <FaDribbbleSquare size={30} className="cursor-pointer hover:scale-105"/>
+                </div>
+            </div>
+            <div className="lg:col-span-2 flex justify-between mt-6">
+                <div>
+                    <h6 className="font-medium text-[#9b9b9b]">Location</h6>
+                    <ul>
+                        <li className="py-2 text-sm">New York</li>
+                        <li className="py-2 text-sm">USA</li>
+                        <li className="py-2 text-sm">India</li>
+                        <li className="py-2 text-sm">Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className="font-medium text-[#9b9b9b]">Location</h6>
+                    <ul>
+                        <li className="py-2 text-sm">New York</li>
+                        <li className="py-2 text-sm">USA</li>
+                        <li className="py-2 text-sm">India</li>
+                        <li className="py-2 text-sm">Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className="font-medium text-[#9b9b9b]">Location</h6>
+                    <ul>
+                        <li className="py-2 text-sm">New York</li>
+                        <li className="py-2 text-sm">USA</li>
+                        <li className="py-2 text-sm">India</li>
+                        <li className="py-2 text-sm">Canada</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className="font-medium text-[#9b9b9b]">Location</h6>
+                    <ul>
+                        <li className="py-2 text-sm">New York</li>
+                        <li className="py-2 text-sm">USA</li>
+                        <li className="py-2 text-sm">India</li>
+                        <li className="py-2 text-sm">Canada</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
